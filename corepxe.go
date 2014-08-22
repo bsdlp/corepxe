bd043b2fbcb6d3933f7af0af572779809a08566f
Jon Chen <bsd@voltaire.sh>
2014-08-22 16:10:26 -0400

    beginnings of the proxy

---
+++ corepxe.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/elazarl/goproxy"
+)
+
+func main() {
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = true
+
+	proxy.OnRequest().DoFunc(
+		func(r *http.Request, _ *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			r.Header.Set("X-COREPXE", "corepxe")
+			return r, nil
+		})
+
+	log.Fatal(http.listenAndServe(":8080", proxy))
+}
