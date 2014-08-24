3b2f9a716ed7a7693df64772b4168888b9e161fd
Jon Chen <bsd@voltaire.sh>
2014-08-24 00:13:01 -0400

    init responseHandler

--- proxy.go
+++ proxy.go
@@ -7,14 +7,15 @@ import (
 	"github.com/elazarl/goproxy"
 )
 
-func proxySetup() {
+func reponseHandler(r *http.Response, ctx *goproxy.ProxyCtx, corpxeChan chan []int) *http.Response {
+	r.Header.Set("X-COREPXE", "corepxe")
+	return r
+}
+
+func proxySetup(corpxeChan chan []int) {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.OnRequest(UrlIs("public.update.core-os.net/v1/update/")).HandleConnect(goproxy.AlwaysMitm)
-	proxy.OnResponse().DoFunc(
-		func(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
-			r.Header.Set("X-COREPXE", "corepxe")
-			return r
-		})
+	proxy.OnResponse().DoFunc(reponseHandler(corpxeChan))
 	proxy.Verbose = true
 	log.Fatal(http.ListenAndServe(":8080", proxy))
 }
