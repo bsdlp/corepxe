c7bf8190f89f71d0a4d397c97300ccc097f40931
Jon Chen <bsd@voltaire.sh>
2014-08-25 13:01:45 -0400

    cpReqHandler stub

--- proxy.go
+++ proxy.go
@@ -7,6 +7,11 @@ import (
 	"github.com/elazarl/goproxy"
 )
 
+func cpReqHandler(r *http.Request, ctx *goproxy.ProxyCtx, corepxeChan chan []int) *http.Request {
+	corepxeChan <- 0
+	return r
+}
+
 func cpRespHandler(r *http.Response, ctx *goproxy.ProxyCtx, corepxeChan chan []int) *http.Response {
 	r.Header.Set("X-COREPXE", "corepxe")
 	return r
@@ -14,7 +19,9 @@ func cpRespHandler(r *http.Response, ctx *goproxy.ProxyCtx, corepxeChan chan []i
 
 func proxySetup(corpxeChan chan []int) {
 	proxy := goproxy.NewProxyHttpServer()
-	proxy.OnRequest(goproxy.UrlIs("public.update.core-os.net/v1/update/")).HandleConnect(goproxy.AlwaysMitm)
+	proxy.OnRequest(goproxy.UrlIs("public.update.core-os.net/v1/update/")).HandleConnect(goproxy.AlwaysMitm).DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) *http.Request {
+		return cpReqHandler(req, ctx, corpxeChan)
+	})
 	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Reponse {
 		return cpRespHandler(resp, ctx, corpxeChan)
 	})
