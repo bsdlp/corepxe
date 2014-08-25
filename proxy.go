7530e3614595d929230a07c027bf72572ea9bf63
Jon Chen <bsd@voltaire.sh>
2014-08-24 23:07:50 -0400

    pass in corepxeChan

--- proxy.go
+++ proxy.go
@@ -7,7 +7,7 @@ import (
 	"github.com/elazarl/goproxy"
 )
 
-func cpRespHandler(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+func cpRespHandler(r *http.Response, ctx *goproxy.ProxyCtx, corepxeChan chan []int) *http.Response {
 	r.Header.Set("X-COREPXE", "corepxe")
 	return r
 }
@@ -16,7 +16,7 @@ func proxySetup(corpxeChan chan []int) {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.OnRequest(goproxy.UrlIs("public.update.core-os.net/v1/update/")).HandleConnect(goproxy.AlwaysMitm)
 	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Reponse {
-		return cpRespHandler(resp, ctx)
+		return cpRespHandler(resp, ctx, corpxeChan)
 	})
 	proxy.Verbose = true
 	log.Fatal(http.ListenAndServe(":8080", proxy))
