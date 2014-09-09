ecb3fbe7639b69deb6630e2d43dbaabd314f7083
Jon Chen <bsd@voltaire.sh>
2014-09-09 19:36:45 -0400

    fix typos

--- proxy.go
+++ proxy.go
@@ -7,15 +7,15 @@ import (
 	"github.com/elazarl/goproxy"
 )
 
-func proxySetup(corpxeChan chan []int) {
+func proxySetup(corepxeChan chan int) {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.OnRequest(goproxy.UrlIs("public.update.core-os.net/v1/update/")).HandleConnect(goproxy.AlwaysMitm).DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) *http.Request {
 		corepxeChan <- 0
-		return r
+		return req
 	})
-	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Reponse {
-		r.Header.Set("X-COREPXE", "corepxe")
-		return r
+	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+		resp.Header.Set("X-COREPXE", "corepxe")
+		return resp
 	})
 	proxy.Verbose = true
 	log.Fatal(http.ListenAndServe(":8080", proxy))
