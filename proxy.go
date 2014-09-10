42a2db05f15638d21c84eda9e42d2e04c7b44ab7
Jon Chen <bsd@voltaire.sh>
2014-09-09 20:36:41 -0400

    corepxeChan is a pointer to the channel

--- proxy.go
+++ proxy.go
@@ -7,11 +7,11 @@ import (
 	"github.com/elazarl/goproxy"
 )
 
-func proxySetup(corepxeChan chan int) {
+func proxySetup(corepxeChan *chan int) {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.OnRequest(goproxy.UrlIs("public.update.core-os.net/v1/update/")).HandleConnect(goproxy.AlwaysMitm)
 	proxy.OnRequest(goproxy.UrlIs("public.update.core-os.net/v1/update/")).DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-		corepxeChan <- 0
+		*corepxeChan <- 0
 		return req, nil
 	})
 	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
@@ -20,4 +20,9 @@ func proxySetup(corepxeChan chan int) {
 	})
 	proxy.Verbose = true
 	log.Fatal(http.ListenAndServe(":8080", proxy))
+}
+
+func main() {
+	corepxeChan := make(chan int)
+	proxySetup(&corepxeChan)
 }
