909ce13293b55aebce399e33e27225b1d70f7fd2
Jon Chen <bsd@voltaire.sh>
2014-09-14 02:20:36 -0400

    do everything in main()

--- main.go
+++ main.go
@@ -8,11 +8,16 @@ import (
 	"github.com/fsouza/go-dockerclient"
 )
 
-func proxySetup(corepxeChan *chan int) {
+func main() {
+	endpoint := "unix:///var/run/docker.sock"
+	client, err := docker.newClient(endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.OnRequest(goproxy.UrlIs("public.update.core-os.net/v1/update/")).HandleConnect(goproxy.AlwaysMitm)
 	proxy.OnRequest(goproxy.UrlIs("public.update.core-os.net/v1/update/")).DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-		*corepxeChan <- 0
 		return req, nil
 	})
 	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
@@ -21,16 +26,5 @@ func proxySetup(corepxeChan *chan int) {
 	})
 	proxy.Verbose = true
 	log.Fatal(http.ListenAndServe(":8080", proxy))
-}
 
-func main() {
-	endpoint := "unix:///var/run/docker.sock"
-	client, err := docker.newClient(endpoint)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	corepxeChan := make(chan int)
-
-	go proxySetup(&corepxeChan)
 }
