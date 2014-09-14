010b2a44160acc38531f9a47d0c9be56c85e3264
Jon Chen <bsd@voltaire.sh>
2014-09-14 02:32:04 -0400

    add ParseRequest

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"encoding/xml"
+	"io/ioutil"
 	"log"
 	"net/http"
 
+	"github.com/coreos/go-omaha/omaha"
 	"github.com/elazarl/goproxy"
 	"github.com/fsouza/go-dockerclient"
 )
 
+func ParseRequest(req *http.Request) omaha.Request {
+	var OmahaRequest omaha.Request
+
+	defer req.Body.Close()
+	data, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if data == nil {
+		log.Fatalln("No request body data.")
+	}
+
+	err = xml.Unmarshal(data, &OmahaRequest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return OmahaRequest
+}
+
 func main() {
 	endpoint := "unix:///var/run/docker.sock"
-	client, err := docker.newClient(endpoint)
+	client, err := docker.NewClient(endpoint)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -18,6 +40,7 @@ func main() {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.OnRequest(goproxy.UrlIs("public.update.core-os.net/v1/update/")).HandleConnect(goproxy.AlwaysMitm)
 	proxy.OnRequest(goproxy.UrlIs("public.update.core-os.net/v1/update/")).DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+		OmahaRequest := ParseRequest(req)
 		return req, nil
 	})
 	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
