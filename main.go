f829fbe9820af4c87ee9e889fa769adff7fc7d63
Jon Chen <bsd@voltaire.sh>
2014-09-15 00:43:02 -0400

    add ParseResponse, add AppsWithUpdates

--- main.go
+++ main.go
@@ -30,7 +30,27 @@ func ParseRequest(req *http.Request) omaha.Request {
 	return OmahaRequest
 }
 
+func ParseResponse(res *http.Response) omaha.Response {
+	var OmahaResponse omaha.Response
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if data == nil {
+		log.Fatalln("No response body data.")
+	}
+
+	err = xml.Unmarshal(data, &OmahaResponse)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return OmahaResponse
+}
+
 func main() {
+	var OriginalRequest http.Request
+	var AppsWithUpdates []omaha.App
 	endpoint := "unix:///var/run/docker.sock"
 	client, err := docker.NewClient(endpoint)
 	if err != nil {
@@ -40,10 +60,20 @@ func main() {
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.OnRequest(goproxy.UrlIs("public.update.core-os.net/v1/update/")).HandleConnect(goproxy.AlwaysMitm)
 	proxy.OnRequest(goproxy.UrlIs("public.update.core-os.net/v1/update/")).DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+		OriginalRequest = req
 		OmahaRequest := ParseRequest(req)
 		return req, nil
 	})
 	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+		OmahaResponse := ParseResponse(resp)
+
+		for i := range OmahaResponse.Apps {
+			if OmahaResponse.Apps[i].Status == "ok" {
+				AppsWithUpdates = append(AppsWithUpdates, OmahaResponse.Apps[i])
+			}
+		}
+		goproxy.NewResponse(OriginalRequest, goproxy.ContentTypeHtml, http.StatusOK, CorePXEResponse)
+
 		resp.Header.Set("X-COREPXE", "corepxe")
 		return resp
 	})
